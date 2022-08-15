3af84229ea0a1aa3db316d5027df38a5a9ab8e09
krzpiszcz <51451493+krzpiszcz@users.noreply.github.com>
2022-08-15 08:24:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3')
+streamlit.text('Kale, Spinach')
+strealit.text('Hard Boilded')
